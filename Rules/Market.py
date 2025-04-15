944120900e285b1a04bb39f2489bde33fb96f36d
im8otto <101478410+im8otto@users.noreply.github.com>
2025-04-15 22:37:23 +0200

    Updated Market API for 8.5.2+
    Updated Market API for 8.5.2+
    Added support for new Market file format (but you can still use also old version)

--- Rules/Market.py
+++ Rules/Market.py
@@ -2,7 +2,7 @@ import json
 import os
 from Utils import Misc
 
-url = "api/v1/inventories"
+url = "/api/v1/dbd-inventories/all"
 filename = "MarketNoSavefile.json"
 status = True
 
@@ -12,14 +12,19 @@ def response(flow):
         if not os.path.exists(market_path):
             Misc.print_log("ERROR: ", market_path, "doesn't exist.")
             return
-
+            
         with open(market_path, "r") as market_file:
             market_json = Misc.normalize_keys(json.load(market_file))
-
+            
+        if len(market_json["data"]["inventory"]) > 0:
+            items = market_json["data"]["inventory"]
+        else:
+            items = market_json["inventoryitems"]
+            
         flow.response.decode()
         response_json = Misc.normalize_keys(json.loads(flow.response.get_text()))
         
-        response_json["data"]["inventory"].extend(market_json["data"]["inventory"])
+        response_json["inventoryitems"].extend(items)
         
         flow.response.text = json.dumps(response_json)
         Misc.print_log("Market successfully loaded")
